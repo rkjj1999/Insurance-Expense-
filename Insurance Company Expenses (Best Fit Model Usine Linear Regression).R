4dd5aca8b06904505b211efd19d21b659cfa65f7
Ronit Kumar <102663135+rkjj1999@users.noreply.github.com>
2022-05-23 22:51:53 +0530

    Add files via upload

---
+++ Insurance Company Expenses (Best Fit Model Usine Linear Regression).R
@@ -0,0 +1,220 @@
+install.packages("caret")
+library("caret")
+
+datas= read.csv("C:/Users/ronit/Downloads/NAICExpense (1) (1).csv")
+dim(datas)
+
+
+## 
+colnames(datas)
+head(datas)
+## 
+
+
+summary(datas)
+
+## Insights:-  
+
+options("scipen"= 100, "digits"= 4)
+
+## Scaling
+attach(datas)
+
+
+exp= EXPENSES*1000
+ls= LONGLOSS*1000
+sl=SHORTLOSS*1000
+gpwp= GPWPERSONAL*1000
+gpwc= GPWCOMM*1000
+asset_s=ASSETS*1000
+cash_s= CASH*1000
+
+datas.scaled= cbind.data.frame(datas, exp, ls, sl, gpwp, gpwc, asset_s,
+                               cash_s)
+
+
+
+colnames(datas.scaled)= c("COMPANY_NAME","GROUP","MUTUAL","STOCK","RBC","STAFFWAGE","AGENTWAGE","LIQUIDRATIO","EXPENSES","LONGLOSS","SHORTLOSS","GPWPERSONAL","GPWCOMM","ASSETS","CASH")
+
+rm( exp, ls, sl, gpwp, gpwc, asset_s, cash_s)
+
+datas= datas.scaled
+dim(datas)
+
+plot(datas)
+plot(datas.scaled)
+
+cor(RBC,STAFFWAGE, method= "spearman" )
+
+
+## Correlation Graphs
+
+install.packages("corrplot")
+library("corrplot")
+
+data(datas)
+corr= cor(datas[,(2:14)])
+corr
+
+corrplot(corr, method= "pie")
+corrplot(corr, method= "color")
+corrplot(corr, method= "number")
+
+head(round(corr,2))
+
+corrplot(corr, type = "upper")
+
+corrplot(corr, type= "lower")
+
+corrplot(corr, type = "upper", col= c("black", "blue"), bg= "lightblue")
+
+
+## Other graphs
+
+library(ggplot2)
+
+grouping(datas)
+
+## Box plot 
+
+
+boxplot(EXPENSES ~GROUP , data = datas, col= "#FFE0B2", "#FFE0B3")
+
+boxplot(EXPENSES ~ MUTUAL , data = datas, col= "#FFE0B2", "#FFE0B3")
+
+## Created a column name TOTAL_ASSET that is the sum of ASSETS and CASH
+## this sum is the sum of fixed and current assets that will be required to compute the relationship with the EXPENSES
+datas$TOTAL_ASSET = (ASSETS+CASH)
+colnames(datas)
+
+boxplot(EXPENSES~CASH, data=datas, main= "Expense Box Plot",
+        xlab="cash",ylab="Expenses")
+
+
+## Pareto Chart
+
+install.packages("qcc")
+library(qcc)
+
+freqdist= table(GROUP)
+pareto.chart(freqdist, main="GROUP pareto Chart", xlab= "GROUP", ylab= "Frequency",
+             col= topo.colors(length(freqdist)))
+
+freqdist1= table(MUTUAL)
+len= length(freqdist1)
+pareto.chart(freqdist1, main= "MUTUAL pareto Chart", col= topo.colors(len))
+freqdist= table(STOCK)
+
+pareto.chart(freqdist, main="STOCK pareto Chart", xlab= "STOCK", ylab= "Frequency",
+             col= topo.colors(length(freqdist)))
+
+## Data Preparation
+
+library("caret")
+dim(datas)
+
+install.packages("tidyverse")
+library(purrr)
+
+map(datas, class)
+
+## Subseting all the unwanted/null columns
+
+colnames(datas)
+datas= subset(datas, select = -c(NA.2))
+datas= subset(datas, select = -c(NA.3))
+datas= subset(datas, select = -c(NA.4))
+datas= subset(datas, select = -c(NA.5))
+datas= subset(datas, select = -c(NA.6))
+datas= subset(datas, select = -c(NA.1))
+datas= subset(datas, select = -c(15))
+datas= subset(datas, select = -c(NA.7))
+colnames(datas)
+
+## Now data is ready for its Modeling
+
+attach(datas)
+datas$Moneytary_Assets = (LIQUIDRATIO*EXPENSES)
+colnames(datas)
+
+## Data Modeling
+
+## Model 1 (Linear regression of EXPENSES & Moneytary Assets)
+
+map(~ lm(Moneytary_Assets~ EXPENSES, data = datas))
+
+
+simple.fit= lm(Moneytary_Assets~LONGLOSS, data= datas)
+summary(simple.fit)
+plot(simple.fit)
+
+plot(Moneytary_Assets~LONGLOSS, data = datasa, pch= 19, col= "red")
+abline(simple.fit, col= "black")
+cor(Moneytary_Assets,LONGLOSS)
+
+multi.fit=lm(Moneytary_Assets~LONGLOSS+SHORTLOSS, data = datas)
+summary(multi.fit)
+plot(multi.fit)
+
+
+## Model 2
+simple.fit= lm(TOTAL_ASSET~RBC, data= datas)
+summary(simple.fit)
+plot(simple.fit)
+
+## Hit <Return> to see next plot: 
+multi.fit= lm(TOTAL_ASSET~LONGLOSS+SHORTLOSS+EXPENSES+GPWPERSONAL+GPWCOMM+STAFFWAGE+AGENTWAGE
+              +RBC, data=datas)
+summary(multi.fit)
+plot(multi.fit)
+
+
+plot(lm(TOTAL_ASSET~RBC, data = datas), pch= 19, col= "red")
+abline(lm(TOTAL_ASSET~RBC), col= "black")
+cor(TOTAL_ASSET,RBC)
+
+
+# 3D Scatterplot
+library(scatterplot3d)
+attach(datasa)
+
+scatterplot3d(TOTAL_ASSET, LONGLOSS,LIQUIDRATIO,
+              main = "3D Scatterplot")
+
+
+library(ggplot2)
+colnames(datasa)
+
+
+## Model 3
+multi.fit= lm(EXPENSES~Moneytary_Assets+TOTAL_ASSET+LONGLOSS+SHORTLOSS+EXPENSES+GPWPERSONAL+GPWCOMM+STAFFWAGE+AGENTWAGE
+              +RBC, data=datasa)
+
+summary(multi.fit)
+plot(multi.fit)
+
+
+## Model 4 (Total asset and Gross premium commercial)
+cor(TOTAL_ASSET,GPWCOMM)
+plot(lm(TOTAL_ASSET~GPWCOMM, data = datas),pch=19, col= "blue")
+multi.fit= lm(TOTAL_ASSET~GPWCOMM+GPWPERSONAL+RBC+EXPENSES)
+summary(multi.fit)
+plot(multi.fit)
+plot(TOTAL_ASSET~GPWCOMM,pch=19, col= "blue")
+abline(lm(TOTAL_ASSET~GPWCOMM, data = datas), col= "red")
+
+plot(lm(TOTAL_ASSET~GPWPERSONAL, data = datas), pch=19, col= "blue")
+abline(lm(TOTAL_ASSET~GPWPERSONAL, data = datas), col= "red")
+
+
+
+## Checking with the heat maps
+library(pheatmap)
+pheatmap(datasa[,8:14], cluster_rows = TRUE, cluster_cols = TRUE, clustering_method = "complete")
+
+
+
+hist(CASH,col = "blue", main="Histogram of Cash and Gross Premium Personal",xlab= "CASH + Gross Premium Personal" ,ylab="Freq")
+hist(GPWPERSONAL,col = "red", main="Histogram of Gross Premium Personal", ylab="Freq", add= TRUE)
+
+
